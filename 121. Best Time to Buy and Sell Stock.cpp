6b3ca4cd9e8e6cd9d61d42221e9568559e2c5c26
Saumyamani Bhardwaz <83209337+Saumyamani-Bhardwaz@users.noreply.github.com>
2023-05-06 23:10:33 +0530

    121. Best Time to Buy and Sell Stock

---
+++ 121. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int buyPrice=INT_MAX;
+        int maxPrice=0;
+        for(int i=0;i<n;i++)
+        {
+            buyPrice=min(buyPrice,prices[i]);
+            maxPrice=max(maxPrice,prices[i]-buyPrice);
+        }
+        cout<<" Saumyamani Bhardwaz_20BCS1682"<<endl;
+        return maxPrice;
+    }
+};
+
